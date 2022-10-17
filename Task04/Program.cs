c75840f4e10c0a0578ad53b93292e29eca9930eb
Sergei Krasnobaev <geekbrainsme@gmail.com>
2022-10-17 15:21:53 +0300

    commit Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -5,10 +5,18 @@ Console.WriteLine("Ведите три целых числа:");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine());
-if(num1>num2);
-    {
-        Console.WriteLine($"Максимальное число : {num1}");
-    }
 
-    
+if(num1>num2 && num1>num3)
+        {
+            Console.WriteLine($"Максимальное число: {num1}");
+        }
+ if(num2>num1 && num2>num3)   
+        {
+            Console.WriteLine($"Максимальное число: {num2}");
+        }
+  if(num3>num1 && num3>num2)
+        {
+            Console.WriteLine($"Максимальное число: {num3}");
+        }
+
     
