2c700351882191bc1b9f9cadc647e1340fa0733c
Sergei Krasnobaev <geekbrainsme@gmail.com>
2022-10-17 14:25:28 +0300

    commit Task16

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,13 +5,24 @@
 //12-> 2
 //85 -> 8
 
-int num = new Random ().Next(10,100);
-Console.WriteLine($"Случайное число из отрезка 10-99={num}");
+//int num = new Random ().Next(10,100);
+//Console.WriteLine($"Случайное число из отрезка 10-99={num}");
+//int FirstDigit = num / 10;
+//int SecondDigit = num % 10;
+//if (FirstDigit>SecondDigit) 
+//Console.WriteLine($"Наибольшая цифра числа ={FirstDigit}");
+//else
+//{
+  //Console.WriteLine($"Наибольшая цифра числа={SecondDigit}");  
+//}
+
+int MaxDigit (int num)
+{
 int FirstDigit = num / 10;
 int SecondDigit = num % 10;
-if (FirstDigit>SecondDigit) 
-Console.WriteLine($"Наибольшая цифра числа ={FirstDigit}");
-else
-{
-  Console.WriteLine($"Наибольшая цифра числа={SecondDigit}");  
+return FirstDigit > SecondDigit ? FirstDigit : SecondDigit;
 }
+int maxDigit = MaxDigit (number);
+Console.WriteLine($"Наибольшая цифра числа ={maxDigit}");
+
+
---
+++ Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Ведите два целых числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+  if (num1*num1==num2 || num2*num2==num1)
+       {if (num1>num2)
+            Console.WriteLine($"число {num1} является квадратом числа {num2}");
+         else
+          {
+            Console.WriteLine($"число {num2} является квадратом числа {num1}");
+          } 
+        }
+  else
+        {
+        Console.WriteLine("ни одно число не является квадратом другого числа");
+        }
