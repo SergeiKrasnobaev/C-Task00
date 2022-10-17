4efa9ab61d6f5d21179666742429a780e4fd7e85
Sergei Krasnobaev <geekbrainsme@gmail.com>
2022-10-17 10:49:52 +0300

    commit Task09

---
+++ Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Ведите три целых числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2);
+    {
+        Console.WriteLine($"Максимальное число : {num1}");
+    }
+
+    
+    
---
+++ Task09/Program.cs
@@ -0,0 +1,17 @@
+﻿// SНапишите программу, которая выводит
+//случайное число из отрезка [10, 99] и показывает
+//наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+int num = new Random ().Next(10,100);
+Console.WriteLine($"Случайное число из отрезка 10-99={num}");
+int FirstDigit = num / 10;
+int SecondDigit = num % 10;
+if (FirstDigit>SecondDigit) 
+Console.WriteLine($"Наибольшая цифра числа ={FirstDigit}");
+else
+{
+  Console.WriteLine($"Наибольшая цифра числа={SecondDigit}");  
+}
